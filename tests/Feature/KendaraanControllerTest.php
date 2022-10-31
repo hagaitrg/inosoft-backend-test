baacb65f0d343f0340717af004019d9fc6df9792
hagaitrg <disahagait@gmail.com>
2022-10-31 10:57:39 +0700

    adding stock test

--- tests/Feature/KendaraanControllerTest.php
+++ tests/Feature/KendaraanControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class KendaraanControllerTest extends TestCase {
     public function testStoreMotor()
@@ -18,9 +19,9 @@ class KendaraanControllerTest extends TestCase {
         ];
 
         $user = User::where('email', 'test@email.com')->first();
-
-        $this->actingAs($user, 'api')
-        ->withSession(['banned'=>false])
+        $token = JWTAuth::fromUser($user);
+        
+        $this->withHeader('Authorization', 'Bearer'. $token)
         ->json('post','/api/v1/kendaraans/motors/store', $payload)
         ->assertStatus(200)
         ->assertJsonStructure([
@@ -37,7 +38,7 @@ class KendaraanControllerTest extends TestCase {
                     '_id'
                 ],
                 [
-                    'motors_id',
+                    'motor_id',
                     'tahun_keluaran',
                     'warna',
                     'harga',
@@ -45,6 +46,71 @@ class KendaraanControllerTest extends TestCase {
                     'created_at',
                     '_id'
                 ]
+            ]
+        ]);
+    }
+
+    public function testStoreMobil()
+    {
+        $payload = [
+            "mesin" => $this->faker->name,
+            "kapasitas_penumpang" => $this->faker->numberBetween(0, 8),
+            "tipe" => $this->faker->name,
+            "tahun_keluaran" => "1995",
+            "warna" => $this->faker->safeColorName(),
+            "harga" => $this->faker->numberBetween(10000000, 100000000)
+        ];
+
+        $user = User::where('email', 'test@email.com')->first();
+        $token = JWTAuth::fromUser($user);
+
+        $this->withHeader('Authorization', 'Bearer'. $token)
+            ->json('post','/api/v1/kendaraans/mobils/store',$payload)
+            ->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'code',
+                    'message',
+                    'data' => [
+                        [
+                            'mesin',
+                            'kapasitas_penumpang',
+                            'tipe',
+                            'updated_at',
+                            'created_at',
+                            '_id'
+                        ],
+                        [
+                            'mobil_id',
+                            'tahun_keluaran',
+                            'warna',
+                            'harga',
+                            'updated_at',
+                            'created_at',
+                            '_id'
+                        ]
+                    ]
+                ]);
+
+    }
+
+    public function testStockKendaraan()
+    {
+        $user = User::where('email', 'test@email.com')->first();
+        $token = JWTAuth::fromUser($user);
+
+        $this->withHeader('Authorization', 'Bearer'. $token)
+        ->json('get', '/api/v1/kendaraans/stock')
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'success',
+            'code',
+            'message',
+            'data' => [
+                'stock kendaraan',
+                'motor',
+                'mobils'
             ]
         ]);
     }
